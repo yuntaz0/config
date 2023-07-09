4b39a5127d95861239e35d951731ad1684825bfd
Yuntao Zhao <yuntaz@proton.me>
2023-07-08 22:33:07 -0700

    Change log file from md to txt
    There is no md format in the output. Txt is better for reader.

--- fish/functions/my-sync.fish
+++ fish/functions/my-sync.fish
@@ -1,5 +1,5 @@
 function my-sync
-    set log_file "log-$(date -u +"%Y-%m-%dT%H:%M:%SZ").md"
+    set log_file "log-$(date -u +"%Y-%m-%dT%H:%M:%SZ").txt"
     set commands "rsync -av --ignore-existing ~/Pictures/ /run/media/me/Framework\ Mobile/Pictures/" "rsync -av --ignore-existing ~/Videos/ /run/media/me/Framework\ Mobile/Videos/" "rsync -av --ignore-existing ~/Music/ /run/media/me/Framework\ Mobile/Music/" "rsync -av --ignore-existing /run/media/me/Framework\ Mobile/Pictures/ ~/Pictures/" "rsync -av --ignore-existing /run/media/me/Framework\ Mobile/Videos/ ~/Videos/" "rsync -av --ignore-existing  /run/media/me/Framework\ Mobile/Music/ ~/Music/"
 
     if test -d "/run/media/me/Framework Mobile"
